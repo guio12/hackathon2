b496de093fd5281d1311ae5a64097d8b772e4893
guio12 <g.audouze@gmail.com>
2018-05-18 10:25:29 +0200

    JQuery page tournoi

---
+++ web/js/tournoi.js
@@ -0,0 +1,80 @@
+
+$(document).ready(function(){
+    $(".match1").on('click', function(){
+        $(".score_match1").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match2").on('click', function(){
+        $(".score_match2").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match3").on('click', function(){
+        $(".score_match3").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match4").on('click', function(){
+        $(".score_match4").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match5").on('click', function(){
+        $(".score_match5").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match6").on('click', function(){
+        $(".score_match6").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match7").on('click', function(){
+        $(".score_match7").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match8").on('click', function(){
+        $(".score_match8").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match9").on('click', function(){
+        $(".score_match9").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match10").on('click', function(){
+        $(".score_match10").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match11").on('click', function(){
+        $(".score_match11").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match12").on('click', function(){
+        $(".score_match12").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match13").on('click', function(){
+        $(".score_match13").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match14").on('click', function(){
+        $(".score_match14").animate({
+            height: 'toggle'
+        });
+    });
+    $(".match15").on('click', function(){
+        $(".score_match15").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+
